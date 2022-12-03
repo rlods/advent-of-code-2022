8b4f13ed598003928f4da08c34ba8e3c8007dd4a
Romain Lods <romain.lods@alan.eu>
2022-12-03 23:45:34 +0100

    Simplify

--- 3/part2.sh
+++ 3/part2.sh
@@ -2,7 +2,7 @@
 
 cat input.txt | xargs -L3 | awk '{
     split($1,a,"")
-    for(i=1;i<=length(a);++i) if(index($2,a[i])>0 && index($3,a[i])>0) {
+    for(i=1;i<=length;++i) if(index($2,a[i])>0 && index($3,a[i])>0) {
         s+=index("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",a[i])
         break
     }
