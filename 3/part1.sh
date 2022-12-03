3d620d3d8928993d2b48c5da4863d50a0bcc7ae0
Romain Lods <romain.lods@alan.eu>
2022-12-03 23:29:10 +0100

    Simplify

--- 3/part1.sh
+++ 3/part1.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 cat input.txt | awk '{   
-    split(substr($0,1,length/2),a,"")
-    b=substr($0,length/2+1)
-    for (i=1;i<=length(a);++i) if (index(b,a[i])>0) {
+    split(substr($0,1,length/2),a,"");b=substr($0,length/2+1)
+    for(i=1;i<=length(a);++i) if(index(b,a[i])>0) {
         s+=index("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",a[i])
         break
     }
