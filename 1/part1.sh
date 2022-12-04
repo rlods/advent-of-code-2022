650aa62cc01c6e1c5921615bd4845c281baff388
Romain Lods <romain.lods@alan.eu>
2022-12-04 12:32:23 +0100

    Simplify

--- 1/part1.sh
+++ 1/part1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat input.txt | awk -v RS= '{for(i=1;i<=NF;i++)a[NR]+=$i} END {for (i in a) {print a[i]}}' | sort -n | tail -1
+cat input.txt | awk -v RS= '{for(i=1;i<=NF;i++)a[NR]+=$i} END {for(i in a) print a[i]}' | sort -n | tail -1
--- 1/part2.sh
+++ 1/part2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat input.txt | awk -v RS= '{for(i=1;i<=NF;i++)a[NR]+=$i} END {for (i in a) {print a[i]}}' | sort -n | tail -3 | awk '{s+=$1} END {print s}'
+cat input.txt | awk -v RS= '{for(i=1;i<=NF;i++)a[NR]+=$i} END {for(i in a) print a[i]}' | sort -n | tail -3 | awk '{s+=$1} END {print s}'
