c132e44bea8a2dd9b07649834e67135592841ad1
Romain Lods <romain.lods@alan.eu>
2022-12-06 17:25:00 +0100

    Day 6

---
+++ 6/part1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat input.txt | awk '{
+    split($1, a, "")
+    for (i=1;i<=length(a);++i){
+        if (i > 4 && --x[a[i-4]] == 0)
+            delete x[a[i-4]]
+        x[a[i]]++
+        if (length(x)==4)
+            break
+    }
+    print i
+}'
---
+++ 6/part2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat input.txt | awk '{
+    split($1, a, "")
+    for (i=1;i<=length(a);++i){
+        if (i > 14 && --x[a[i-14]] == 0)
+            delete x[a[i-14]]
+        x[a[i]]++
+        if (length(x)==14)
+            break
+    }
+    print i
+}'
