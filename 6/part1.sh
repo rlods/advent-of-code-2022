294b5e7ae6ad78deb8f5fcd4c794eec734dfbe5b
Romain Lods <romain.lods@alan.eu>
2022-12-06 17:26:24 +0100

    Simplify

--- 6/part1.sh
+++ 6/part1.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cat input.txt | awk '{
-    split($1, a, "")
+    split($1,a,"")
     for (i=1;i<=length(a);++i){
-        if (i > 4 && --x[a[i-4]] == 0)
+        if (i>4 && --x[a[i-4]]==0)
             delete x[a[i-4]]
         x[a[i]]++
         if (length(x)==4)
--- 6/part2.sh
+++ 6/part2.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cat input.txt | awk '{
-    split($1, a, "")
+    split($1,a,"")
     for (i=1;i<=length(a);++i){
-        if (i > 14 && --x[a[i-14]] == 0)
+        if (i>14 && --x[a[i-14]]==0)
             delete x[a[i-14]]
         x[a[i]]++
         if (length(x)==14)
