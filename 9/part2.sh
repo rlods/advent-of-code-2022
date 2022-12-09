ddeed2002f90b03d32a5318429ac4d98fc2e4cf7
Romain Lods <romain.lods@alan.eu>
2022-12-09 23:35:35 +0100

    Cleanup

--- 9/part2.sh
+++ 9/part2.sh
@@ -2,16 +2,18 @@
 
 cat input.txt | awk '
 BEGIN {
-    for (i=0; i<=9; ++i)
+    H = 0
+    T = 9
+    for (i=H; i<=T; ++i)
         x[i]=y[i]=0
     a[0,0]=1
 } {
     for (k=0; k<$2; ++k) {
-        if      ($1=="R") x[0]++
-        else if ($1=="L") x[0]--
-        else if ($1=="U") y[0]++
-        else if ($1=="D") y[0]--
-        for (i=1;i<=9;++i) {
+        if      ($1=="R") x[H]++
+        else if ($1=="L") x[H]--
+        else if ($1=="U") y[H]++
+        else if ($1=="D") y[H]--
+        for (i=H+1; i<=T; ++i) {
             s2 = (x[i-1] - x[i]) ** 2 + (y[i-1] - y[i]) ** 2
             if (s2 > 2) {
                 s = sqrt(s2)
@@ -29,7 +31,7 @@ BEGIN {
                 else if (d=="⬊") {x[i]++; y[i]--}
                 else if (d=="⬋") {x[i]--; y[i]--}
 
-                if (i==9)
+                if (i==T)
                     a[x[i],y[i]]=1
             }
         }
@@ -42,14 +44,14 @@ BEGIN {
 # Y1=-20;Y2=20;X1=-20;X2=20
 # for (Y=Y2;Y>=Y1;--Y) {
 #     for (X=X1;X<=X2;++X) {
-#         for (o=0;o<10;++o) {
+#         for (o=H;o<T+1;++o) {
 #             if (X==x[o] && Y==y[o]) {
-#                 if (o==0) printf("H")
+#                 if (o==H) printf("H")
 #                 else printf(o)
 #                 break
 #             }
 #         }
-#         if (o==10)
+#         if (o==T+1)
 #             printf(X==0&&Y==0?"s":".")
 #     }
 #     printf("\n")
