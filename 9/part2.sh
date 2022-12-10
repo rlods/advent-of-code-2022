73749843ad5125cf7ce84533aaeba70d10a8f48a
Romain Lods <romain.lods@alan.eu>
2022-12-10 01:10:09 +0100

    Cleanup

--- 9/part2.sh
+++ 9/part2.sh
@@ -4,23 +4,23 @@ cat input.txt | awk '
 BEGIN {
     H = 0
     T = 9
-    for (k=H; k<=T; ++k)
-        x[k]=y[k]=0
-    a[0,0]=1
+    for (k = H; k <= T; ++k)
+        x[k] = y[k] = 0
+    a[0,0] = 1
 } {
-    for (s=0; s<$2; ++s) {
-        if      ($1=="U") y[H]++
-        else if ($1=="D") y[H]--
-        else if ($1=="R") x[H]++
-        else if ($1=="L") x[H]--
-        for (k=H+1; k<=T; ++k) {
+    for (s = 0; s < $2; ++s) {
+        if      ($1 == "U") y[H]++
+        else if ($1 == "D") y[H]--
+        else if ($1 == "R") x[H]++
+        else if ($1 == "L") x[H]--
+        for (k = H+1; k <= T; ++k) {
             dx = x[k-1] - x[k]
             dy = y[k-1] - y[k]
             if (dx ** 2 + dy ** 2 > 2) {
-                # (A>0) - (A<0) is a way to get the sign of A ie. -1, 0 or 1
+                # (A>0) - (A<0) is a funny way to get the sign of A ie. -1, 0 or 1
                 x[k] += (dx>0) - (dx<0)
                 y[k] += (dy>0) - (dy<0)
-                if (k==T) a[x[k],y[k]]=1
+                if (k == T) a[x[k],y[k]]=1
             }
         }
     }
