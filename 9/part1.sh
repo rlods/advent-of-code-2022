597806ee963a22d3c268c5397d5be89d2fd9cde1
Romain Lods <romain.lods@alan.eu>
2022-12-10 01:24:14 +0100

    Comment

--- 9/part1.sh
+++ 9/part1.sh
@@ -17,7 +17,7 @@ BEGIN {
             dx = x[k-1] - x[k]
             dy = y[k-1] - y[k]
             if (dx ** 2 + dy ** 2 > 2) {
-                # (A>0) - (A<0) is a funny way to get the sign of A ie. -1, 0 or 1
+                # (N>0) - (N<0) is a funny way to get the sign of N ie. -1, 0 or 1
                 x[k] += (dx>0) - (dx<0)
                 y[k] += (dy>0) - (dy<0)
                 if (k == T) a[x[k],y[k]]=1
