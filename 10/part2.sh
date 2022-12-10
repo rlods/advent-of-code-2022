7fe8d375f5e68109b56a8d596391d60ee097fab3
Romain Lods <romain.lods@alan.eu>
2022-12-10 23:14:45 +0100

    Cleanup

--- 10/part2.sh
+++ 10/part2.sh
@@ -4,9 +4,17 @@ awk 'BEGIN{x=1}{
     d=$1=="addx"?2:1
     for(i=0;i<d;++i){
         if(c%240==0){b=0;printf("\033c")}
-        ++c
-        printf(b+x==c||b+x+1==c||b+x+2==c?"#":".")
+        ++c;t=c-x-b
+        printf(t==0||t==1||t==2?"#":".")
         if(c%40==0){b+=40;printf("\n")}
     }
     if($1=="addx")x+=$2
 }' input.txt
+
+
+# Replaced
+# printf(b+x==c||b+x+1==c||b+x+2==c?"#":".")
+# By
+# t=c-x-b
+# printf(t==0||t==1||t==2?"#":".")
+
