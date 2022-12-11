7a87b1289de944b5310af0a11ea6a72766acc1fd
Romain Lods <romain.lods@alan.eu>
2022-12-11 20:36:02 +0100

    Day 11

---
+++ 11/part2.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+awk -v R=10000 '
+/M/{M=int(substr($2,1,length($2)-1))}
+/S/{for(i=3;i<=NF;++i)I[M]=I[M] $i}
+/O/{O[M]=$5;V[M]=$6}
+/T/{D[M]=$4}
+/tr/{T[M]=$6}
+/fa/{F[M]=$6}
+func _gcd(a,b){return b?_gcd(b,a%b):a}
+func _lcm(a,b){return (a*b)/_gcd(a,b)}
+END{
+    lcm=D[0]
+    for(m=0;m<=M;++m)lcm=_lcm(lcm,D[m])
+    for(r=1;r<=R;++r)for(m=0;m<=M;++m){
+        split(I[m],it,",");delete I[m]
+        for(i=1;i<=length(it);++i){
+            v=it[i]
+            x=V[m]=="old"?v:V[m]
+            if(O[m]=="+")v+=x;else v*=x
+            v%=lcm
+            d=!(v%D[m])
+            if(d)I[T[m]]=I[T[m]]?I[T[m]]","v:v
+            else I[F[m]]=I[F[m]]?I[F[m]]","v:v
+            MC[m]++
+        }
+    }
+    m1=m2=0
+    for(m=0;m<=M;++m)
+        if(MC[m]>m1){m2=m1;m1=MC[m]}
+        else if(MC[m]>m2)m2=MC[m]
+    print m1*m2
+}' input.txt
+
+
