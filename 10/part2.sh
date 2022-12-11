ef87aa1d952d0d112a6bce6b4d0ff4db49ee201f
Romain Lods <romain.lods@alan.eu>
2022-12-11 09:33:13 +0100

    Cleanup

--- 10/part2.sh
+++ 10/part2.sh
@@ -4,8 +4,8 @@ awk 'BEGIN{x=1}{
     d=$1=="addx"?2:1
     for(i=0;i<d;++i){
         if(c%240==0){b=0;printf("\033c")}
-        ++c;t=c-x-b
-        printf(t==0||t==1||t==2?"#":".")
+        ++c;t=c-x-b+1
+        printf(t>0&&t<4?"#":".")
         if(c%40==0){b+=40;printf("\n")}
     }
     if($1=="addx")x+=$2
@@ -16,5 +16,5 @@ awk 'BEGIN{x=1}{
 # printf(b+x==c||b+x+1==c||b+x+2==c?"#":".")
 # By
 # t=c-x-b
-# printf(t==0||t==1||t==2?"#":".")
+# printf(t>0&&t<4?"#":".")
 
